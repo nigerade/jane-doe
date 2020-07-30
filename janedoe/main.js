db401d36bb468a5198254cf5578c88848fb52825
nigerade <64471143+nigerade@users.noreply.github.com>
2020-07-30 20:12:41 -0300

    Add files via upload

---
+++ janedoe/main.js
@@ -0,0 +1,10 @@
+$("#my-form").submit(function(e) {
+    e.preventDefault();
+
+    var $form = $(this);
+    $.post($form.attr("action"), $form.serialize()).then(function() {
+        alert("Thank you!");
+    });
+
+    window.location.reload();
+});
